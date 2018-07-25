47960a517d0dc8feca92fc4ed50493152a421f41
Muharrizk Abilaksana <ai.mo7g@gmail.com>
2018-07-25 16:52:28 +0700

    admob not showing

--- src/boot.js
+++ src/boot.js
@@ -5,6 +5,9 @@ CarFreeDay.Boot.prototype = {
     preload: function(){
         this.load.image('logo','assets/images/preload_bg.png');
         this.load.image('preloadbar','assets/images/preload_bar.png');
+
+        banner.load();
+        banner.setLayout(Cocoon.Ad.BannerLayout.TOP_CENTER);
     },
     create: function(){
         this.game.stage.backgroundColor = '#000000';
